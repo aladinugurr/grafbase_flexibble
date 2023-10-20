3c915644dac6bc9a50109df8805ba35313888437
aladinugurr <ugur.cata6@gmail.com>
2023-10-20 23:03:59 +0300

    create project to database

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -6,23 +6,37 @@ import FormField from './FormField'
 import { categoryFilters } from '@/constant'
 import CustomMenu from './CustomMenu'
 import Button from './Button'
+import { createNewProject, fetchToken } from '@/lib/actions'
+import { useRouter } from 'next/navigation'
 
 type Props = {
 	type: string,
 	session: SessionInterface
 }
 
+
+
 const ProjectForm = ({type, session}: Props) => {
-	const handleSubmit = (e: React.FormEvent) => {
+	const router = useRouter();
+	
+
+	const handleSubmit = async(e: React.FormEvent) => {
 		e.preventDefault();
 
 		setIsSubmitting(true);
+		const {token} = await fetchToken();
 		try {
 			if(type === 'create') {
 				//create project
-			}
-		} catch {
+				await createNewProject(form, session?.user?.id, token)
 
+				router.push('/');
+			}
+		} catch(error) {
+			console.log(error);
+			
+		} finally {
+			setIsSubmitting(false);
 		}
 	}
 
